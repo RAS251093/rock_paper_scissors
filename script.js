8df3bf42f051a8e34031b36be22ac04afd6d3304
Rob Smith <r.smith251093@gmail.com>
2022-06-08 22:13:21 +0100

    Adds round and total game win condition functions

--- script.js
+++ script.js
@@ -9,3 +9,54 @@ function computerPlay () {
          return "Scissors";
     }
 }
+
+function playRound (playerSelection, computerSelection) {
+
+    let winTest = (playerSelection + computerSelection).toLowerCase();
+
+    switch(winTest) {
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+            return "draw";
+            break;
+        case "rockpaper":
+        case "paperscissors":
+        case "scissorsrock":
+            return "lose"
+            break;
+        case "rockscissors":
+        case "paperrock":
+        case "scissorspaper":
+            return "win"
+            break;
+    }
+}
+
+function game() {
+    let total = 0;
+    for (let i = 0; i < 5; i++) {
+        if (playRound(playerSelection, computerPlay()) === "win") {
+            total += 1;
+        } else if (playRound(playerSelection, computerPlay()) === "lose") {
+            total -= 1;
+        } else {
+            total += 0;
+        }
+    }
+    console.log(total);
+    if (total > 0) {
+        return "You got the most wins over 5 rounds, you win!!";
+    } else if (total < 0) {
+        return "You got the most losses over 5 rounds, you lose...";
+    } else {
+        return "Over the 5 rounds played, it was a draw!";
+    }
+}
+
+
+const playerSelection = "rock";
+
+    
+console.log(game());
+
